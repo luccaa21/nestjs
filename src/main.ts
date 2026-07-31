617e32198c71b5353a11527afbee62095c9ebda9
luccaa21 <194046@upf.br>
2026-07-30 21:44:03 -0300

    feat: implemented swagger visualization

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // IMPLEMENTAÇÃO SWAGGER
+  // 1. cria a configuração básica
+  const config = new DocumentBuilder()
+    .setTitle('API NestJS')
+    .setDescription('Documentação da API criada em aula')
+    .setVersion('1.0')
+    .build();
+  
+  // 2. gera o documento
+  const document = SwaggerModule.createDocument(app, config);
+  
+  // 3. monta o Swagger na rota '/api'
+  SwaggerModule.setup('api', app, document);
+
 }
 bootstrap();
